bb6f3d2d70e27adcabb7f7c556abb2b65b72e36d
Taito Horiuchi <taito.horiuchi@gmail.com>
2012-11-08 23:31:03 +0200

    Updated templates.

--- slt/theme/browser/template.py
+++ slt/theme/browser/template.py
@@ -1,5 +1,9 @@
+from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone.interfaces import IPloneSiteRoot
+from collective.cart.core.browser.base import BaseListingObject
+from collective.cart.shopping.interfaces import ICart
 from five import grok
+from plone.app.contentlisting.interfaces import IContentListing
 from slt.theme.browser.interfaces import ISltThemeLayer
 from slt.content.schema import IMemberArea
 
@@ -21,15 +25,44 @@ class ShopView(BaseView):
     grok.template('shop')
 
 
-class AddressListView(BaseView):
-    """View for listing addresses for MemberArea."""
+class BaseListView(BaseView):
+    """Base view for listing for MemberArea."""
+    grok.baseclass()
     grok.context(IMemberArea)
+
+    def update(self):
+        self.request.set('disable_plone.leftcolumn', True)
+        self.request.set('disable_plone.rightcolumn', True)
+
+
+class AddressListView(BaseListView):
+    """View for listing addresses for MemberArea."""
     grok.name('addresses')
     grok.template('addresses')
 
+    def addresses(self):
+        return []
 
-class OrderListView(BaseView):
+
+class OrderListView(BaseListView, BaseListingObject):
     """View for listing orders for MemberArea."""
-    grok.context(IMemberArea)
-    grok.name('orders')
+    grok.name('view')
     grok.template('orders')
+
+    def carts(self):
+        catalog = getToolByName(self.context, 'portal_catalog')
+        query = {
+            'Creator': self.context.getOwner().getId(),
+            'object_provides': ICart.__identifier__,
+        }
+        result = []
+        for item in IContentListing(catalog(query)):
+            res = {
+                'id': item.getId(),
+                'title': item.Title(),
+                'url': item.getURL(),
+                'review_state': item.review_state(),
+                'modified': self._localized_time(item),
+            }
+            result.append(res)
+        return result
