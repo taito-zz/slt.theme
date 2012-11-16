c8586b5c4e179f62ddfaaef8566dc33249862df9
Taito Horiuchi <taito.horiuchi@gmail.com>
2012-11-16 23:37:34 +0200

    Updated template.

--- slt/theme/browser/template.py
+++ slt/theme/browser/template.py
@@ -1,12 +1,17 @@
 from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone.interfaces import IPloneSiteRoot
 from collective.cart.core.browser.base import BaseListingObject
+from collective.cart.core.interfaces import IBaseAdapter
 from collective.cart.shopping.interfaces import ICart
+from collective.cart.shopping.interfaces import ICartAdapter
 from five import grok
 from plone.app.contentlisting.interfaces import IContentListing
 from slt.content.interfaces import IMember
 from slt.content.schema import IMemberArea
 from slt.theme.browser.interfaces import ISltThemeLayer
+from slt.theme.interfaces import ICollapsedOnLoad
+from zope.component import getMultiAdapter
+from zope.component import getUtility
 
 
 grok.templatedir('templates')
@@ -42,25 +47,28 @@ class AddressListView(BaseListView):
         return IMember(self.context).infos
 
 
-class OrderListView(BaseListView, BaseListingObject):
+class OrderListView(BaseListView):
     """View for listing orders for MemberArea."""
     grok.name('view')
     grok.template('orders')
 
     def carts(self):
-        catalog = getToolByName(self.context, 'portal_catalog')
-        query = {
-            'Creator': self.context.getOwner().getId(),
-            'object_provides': ICart.__identifier__,
-        }
-        result = []
-        for item in IContentListing(catalog(query)):
-            res = {
+        base = IBaseAdapter(self.context)
+        res = []
+        creator = getMultiAdapter((self.context, self.request), name="plone_portal_state").member().id
+        for item in base.get_content_listing(ICart, Creator=creator, path='/'):
+            obj = item.getObject()
+            cart = ICartAdapter(obj)
+            res.append({
+                'articles': cart.articles,
                 'id': item.getId(),
-                'title': item.Title(),
-                'url': item.getURL(),
+                'modified': base.localized_time(item),
                 'review_state': item.review_state(),
-                'modified': self._localized_time(item),
-            }
-            result.append(res)
-        return result
+                'title': item.Title(),
+                'total': cart.total,
+                'url': item.getURL(),
+            })
+        return res
+
+    def class_collapsible(self):
+        return getUtility(ICollapsedOnLoad)()
