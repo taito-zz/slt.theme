42abc6def3d5757513be0a000caa80fbb347288f
Taito Horiuchi <taito.horiuchi@gmail.com>
2012-12-20 06:59:38 +0900

    Release 0.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='slt.theme',
-    version='0.5.1',
+    version='0.6',
     description="Turns plone them into SLT shopping theme.",
     long_description=open("README.rst").read(),
     classifiers=[
